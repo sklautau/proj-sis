24d34733c64b5ab4c62865c32bf706cbc4d9e079
Xalkaszczan <vasson.arthur@gmail.com>
2022-02-09 14:37:37 +0100

    id
    création de l'id via la méthode DICOM

--- ProjetSIS/src/NoyauFonctionnel/Acces.java
+++ ProjetSIS/src/NoyauFonctionnel/Acces.java
@@ -15,22 +15,36 @@ public class Acces {
 public static void AjoutPatient(Connection conn) throws SQLException {
 // Get a statement from the connection
 Statement st = conn.createStatement();
-// A CHANGER JE VEUX POUVOIR LE FAIRE AUTOMATIQUEMENT//
-String update ="";
-String Nom = "Vasson";
-String Prenom = "Arthur";
-Date datenais = new Date(2000,02,29);
-int nb = st.executeUpdate("INSERT INTO personne(Id,Nom,prenom,Datenais,Adresse) VALUES(‘id’, ‘prenom’,'nom','datenais','adresse')");
+// A CHANGER JE VEUX POUVOIR LE FAIRE AUTOMATIQUEMENT AVEC LE BOUTON//
+String Nom = "Vasson";//getnom
+String Prenom = "Arthur";//getprenom
+Date Datenais = new Date(2000,02,29);//getdatenais
+String Adresse = "21 rue de Verlande";//getadresse
+String Type = "testType";
+String Passif ="testPassif";
+String id =""+Passif+""+Type;
+// ATTENTION DOUBLONS//
 ////
-System.out.println("Nombre de lignes insérées = " + nb);
+String update ="INSERT INTO personne(Id,Nom,prenom,Datenais,Adresse,Type,Passif) VALUES('"+id+"','"+Nom+"','"+Prenom+"','"+Datenais+"','"+Adresse+"','"+Type+"','"+Passif+"')";
+int nb = st.executeUpdate(update);
+System.out.println("Nombre de lignes insérées = " + nb);//retours du nombre de lignes, secondaire
 st.close();
 }
 public static void LecturePatient(Connection conn) throws SQLException {
 try {
 // create new statement
 Statement st = conn.createStatement();
-String query = "SELECT Nom,Prenom,Datenais,Adresse FROM personne";
+///à obtenir automatiquement aussi
+String Nom = "Vasson";//getnom
+String Prenom = "Arthur";//getprenom
+Date Datenais = new Date(2000,02,29);//getdatenais
+String Adresse = "21 rue de Verlande";//getadresse
+////
+String query = "SELECT "+Nom+","+Prenom+","+Datenais+","+Adresse+" FROM personne";
+//String query = "SELECT Nom,Prenom,Datenais,Adresse FROM personne";
+
 ResultSet rs = st.executeQuery(query);
+
 while (rs.next()) {
 String nom = rs.getString(1);
 String prenom = rs.getString(2);
@@ -46,7 +60,92 @@ conn.close();
 }
 }
 
+public static void AjoutDMR(Connection conn) throws SQLException {
+// Get a statement from the connection
+Statement st = conn.createStatement();
+// A CHANGER JE VEUX POUVOIR LE FAIRE AUTOMATIQUEMENT AVEC LE BOUTON//
+Date date = new Date(2000,02,29);//getdate
+String PH = "Arthur";//getprenom
+String type = "Cardiologie";//getdatenais
+String PACS = "UID";//getadresse
+////
+String update ="INSERT INTO DMR(date,PH,type,PACS) VALUES('"+date+"','"+PH+"','"+type+"','"+PACS+"',')";
+int nb = st.executeUpdate(update);
+System.out.println("Nombre de lignes insérées = " + nb);//retours du nombre de lignes, secondaire
+st.close();
+}
+public static void LectureDMR(Connection conn) throws SQLException {
+try {
+// create new statement
+Statement st = conn.createStatement();
+///à obtenir automatiquement aussi
+String Nom = "Vasson";//getnom
+String Prenom = "Arthur";//getprenom
+Date Datenais = new Date(2000,02,29);//getdatenais
+String Adresse = "21 rue de Verlande";//getadresse
+////
+String query = "SELECT "+Nom+","+Prenom+","+Datenais+","+Adresse+" FROM personne";
+//String query = "SELECT Nom,Prenom,Datenais,Adresse FROM personne";
 
+ResultSet rs = st.executeQuery(query);
 
+while (rs.next()) {
+String nom = rs.getString(1);
+String prenom = rs.getString(2);
+java.sql.Date DateNais = rs.getDate(3);//format d'une date à chercher 
+String adresse = rs.getString(4);   
 
+System.out.printf("%-20s | %-20s | %3d\n", nom, prenom, DateNais,adresse);
+}
+} finally {
+// close statement and connection
+if (conn != null)
+conn.close();
+}
+}
+
+public static void AjoutExamen(Connection conn) throws SQLException {
+// Get a statement from the connection
+Statement st = conn.createStatement();
+// A CHANGER JE VEUX POUVOIR LE FAIRE AUTOMATIQUEMENT AVEC LE BOUTON//
+String Nom = "Vasson";//getnom
+String Prenom = "Arthur";//getprenom
+Date Datenais = new Date(2000,02,29);//getdatenais
+String Adresse = "21 rue de Verlande";//getadresse
+////
+String update ="INSERT INTO personne(Id,Nom,prenom,Datenais,Adresse) VALUES('1','"+Nom+"','"+Prenom+"','"+Datenais+"','"+Adresse+"')";
+int nb = st.executeUpdate(update);
+System.out.println("Nombre de lignes insérées = " + nb);//retours du nombre de lignes, secondaire
+st.close();
+}
+
+public static void LectureExamen(Connection conn) throws SQLException {
+try {
+// create new statement
+Statement st = conn.createStatement();
+///à obtenir automatiquement aussi
+String Nom = "Vasson";//getnom
+String Prenom = "Arthur";//getprenom
+Date Datenais = new Date(2000,02,29);//getdatenais
+String Adresse = "21 rue de Verlande";//getadresse
+////
+String query = "SELECT "+Nom+","+Prenom+","+Datenais+","+Adresse+" FROM personne";
+//String query = "SELECT Nom,Prenom,Datenais,Adresse FROM personne";
+
+ResultSet rs = st.executeQuery(query);
+
+while (rs.next()) {
+String nom = rs.getString(1);
+String prenom = rs.getString(2);
+java.sql.Date DateNais = rs.getDate(3);//format d'une date à chercher 
+String adresse = rs.getString(4);   
+
+System.out.printf("%-20s | %-20s | %3d\n", nom, prenom, DateNais,adresse);
+}
+} finally {
+// close statement and connection
+if (conn != null)
+conn.close();
+}
+}
 }
