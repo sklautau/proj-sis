f138de4f86b479690987da0ab119f3baaff956c2
S. Klautau <56610284+sklautau@users.noreply.github.com>
2022-03-23 15:49:44 +0100

    menu deroulant recherche examen

--- ProjetSIS/src/Interface/RechercheExamen.java
+++ ProjetSIS/src/Interface/RechercheExamen.java
@@ -56,12 +56,12 @@ public class RechercheExamen extends javax.swing.JFrame {
 
         jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
         jTextField2 = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
+        jComboBox1 = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(425, 250));
@@ -99,6 +99,8 @@ public class RechercheExamen extends javax.swing.JFrame {
             }
         });
 
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "ANGIOCTSCANNER", "ANGIOIRM", "ANDIODIGITALISEE", "ARTHROSCANNER", "BIOPSIESEIN", "BIOPSIEFLUOROSCANNER", "CHOLANGIOIRM", "COLONLAVEMENT", "CTCOLONOGRAPHIE", "CTCORONOGRAPHIE", "CTSCANNERVOLUMIQUE", "CYSTOGRAPHIESUSPUBIENNE", "DEFECOIRM", "DENTASCANNER", "ECHOGRAPHIEREFLUXPEDIATRIQUE", "ECHOGRAPHIEDOPPLER", "ECHOGRAPHIE", "ENTEROCTSCANNER", "HYSTEROGRAPHIE", "INFILTRATIONINTERAPOPHYSAIRE", "INFILTRATIONPERIRADICULAIRE", "IRM", "MAMMOTEST", "OEDTRANSIT", "OSTEODENSITOMETRIE", "PELVIMETRIE", "PONCTIONTHYROIDE", "RADIOLOGIECONVENTIONNELLE", "SENOLOGIE", "UROGRAPHIEIV" }));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -117,8 +119,8 @@ public class RechercheExamen extends javax.swing.JFrame {
                                     .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 228, Short.MAX_VALUE)
-                                    .addComponent(jTextField2)))
+                                    .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 228, Short.MAX_VALUE)
+                                    .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                             .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                                 .addGap(242, 242, 242)
                                 .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -140,13 +142,10 @@ public class RechercheExamen extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton3)
                     .addComponent(jButton1))
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(25, 25, 25)
-                        .addComponent(jLabel1))
-                    .addGroup(layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(22, 22, 22)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
@@ -157,8 +156,6 @@ public class RechercheExamen extends javax.swing.JFrame {
                 .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
-
-        layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {jTextField1, jTextField2});
 
         layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {jLabel1, jLabel2, jLabel3});
 
@@ -204,7 +201,7 @@ public class RechercheExamen extends javax.swing.JFrame {
 
          String p=" ";
         
-        try {p = Acces.LectureExamen(jTextField1.getText(),jTextField2.getText(),s,k);
+        try {p = Acces.LectureExamen(jComboBox1.getSelectedItem().toString(),jTextField2.getText(),s,k);
         } 
         catch (SQLException ex) {
             Logger.getLogger(AjouterPatient.class.getName()).log(Level.SEVERE, null, ex);
@@ -264,10 +261,10 @@ public class RechercheExamen extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
 }
